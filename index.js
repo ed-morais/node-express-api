37ceff974441cffb2f78ad3ef714b3067a68bb5b
Edilson Morais <moraisbritoedilson@gmail.com>
2023-03-10 17:32:34 -0300

    feat: add users.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
+
+import usersRoutes from "./routes/users.js";
 
 const app = express();
 const PORT = 5050;
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=> {res.send('Hello from homepage')});
+app.use("/users", usersRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`))
+app.get("/", (req, res) => {
+  res.send("Hello from homepage");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server running on port: http://localhost:${PORT}`)
+);
