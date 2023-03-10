d9487c3e60bff634ef509070d77331a2f3738293
Edilson Morais <moraisbritoedilson@gmail.com>
2023-03-10 17:59:05 -0300

    feat: add post request to users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
+uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 25,
+  },
+];
+
 router.get("/", (req, res) => {
-  res.send("hello");
+  res.send(users);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+
+  users.push(user);
+  // users.push()
+  res.send(`User with the name ${user.firstName} added to the database`);
 });
 
 export default router;
